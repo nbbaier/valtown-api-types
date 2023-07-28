ff0b82435588e484c5d9a42b3d6bcb005eef051e
Nico Baier <nico.baier@gmail.com>
2023-07-28 18:29:09 -0500

    fixed typo

--- generator.js
+++ generator.js
@@ -2,12 +2,9 @@ import fs from "fs";
 import yaml from "yaml";
 import fetch from "node-fetch";
 
-let yamlString = await fetch("https://www.val.town/docs/openapi.yaml").then(
+const yamlString = await fetch("https://www.val.town/docs/openapi.yaml").then(
   (response) => response.text()
 );
 
-console.log(yamlString);
-
 const jsonString = JSON.stringify(yaml.parse(yamlString));
-Should;
 fs.writeFileSync("./api.ts", `export const api = ${jsonString} as const`);
