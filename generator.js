ba7d194aff81ed80038fc0d547d8a47d9f504080
Nico Baier <nico.baier@gmail.com>
2023-07-28 15:54:38 -0500

    initial commit

---
+++ generator.js
@@ -0,0 +1,10 @@
+import fs from "fs";
+import yaml from "yaml";
+import fetch from "node-fetch";
+
+const yamlString = await fetch("https://www.val.town/docs/openapi.yaml").then(
+  (res) => res.text()
+);
+const jsonString = JSON.stringify(yaml.parse(yamlString));
+
+fs.writeFileSync("./api.ts", `export const api = ${jsonString} as const`);
