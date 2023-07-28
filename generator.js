ecdab3085128b3b3ba27a4e871f3eb8f583aab03
Nico Baier <nico.baier@gmail.com>
2023-07-28 18:23:30 -0500

    -m

--- generator.js
+++ generator.js
@@ -2,9 +2,12 @@ import fs from "fs";
 import yaml from "yaml";
 import fetch from "node-fetch";
 
-const yamlString = await fetch("https://www.val.town/docs/openapi.yaml").then(
-  (res) => res.text()
-);
+const yamlString = await fetch(
+  "https://api.val.town/v1/run/stevekrouse.vtOpenAPI"
+).then((response) => response.json());
+
+console.log(yamlString);
+
 const jsonString = JSON.stringify(yaml.parse(yamlString));
 
 fs.writeFileSync("./api.ts", `export const api = ${jsonString} as const`);
